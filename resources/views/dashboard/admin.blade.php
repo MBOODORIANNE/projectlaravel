8c73a61dd06ab070d83d8135f5a2982467427148
steeven <samendjiaha@gmail.com>
2025-07-17 21:55:42 +0100

    Amélioration de la mise en page et de la structure du tableau de bord administrateur : simplification des commentaires CSS, ajustement des marges et correction de l'espacement dans le menu de navigation.

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -4,7 +4,6 @@
 
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
 <style>
     /* ---------------------
        Sidebar (navigation)
